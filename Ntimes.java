f3e36d056902b4d1621c9ef8f3468ad1b6a0f1a0
amansaraswat <32701934+amansaraswat@users.noreply.github.com>
2018-01-25 22:27:23 +0530

    Create Ntimes.java

---
+++ Ntimes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Ntimes
+{
+	public static void main(String ag[])
+	{
+		Scanner obj=new Scanner(System.in);
+		int N=obj.nextInt();
+		if(N>0)
+		  for(int i=1;i<=N;i++)
+		  {
+		  	System.out.println("Hello");
+		  }
+		else
+		  System.out.print("invalid input");
+	}
+	
+}
