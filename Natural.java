a0103d3e6d532a850a39ce2e346b69b48b44032c
amansaraswat <32701934+amansaraswat@users.noreply.github.com>
2018-01-25 22:48:19 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Natural
+{
+	public static void main(String ag[])
+	{
+		Scanner obj=new Scanner(System.in);
+		int N=obj.nextInt();
+		int sum=0;
+    if(N>=0)
+		{
+    while(N!=0)
+        {
+          sum+=N;
+			N--;
+        }
+		System.out.println(sum);
+    }
+    else
+     Syatem.out.println("invalid input");
+ 	}
+	
+}
